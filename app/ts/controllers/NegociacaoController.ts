f6df97300bf22fc5336f12d193d83ca909da9c7e
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-05-26 00:00:04 -0300

    Update view

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -11,6 +11,8 @@ class NegociacaoController {
         this._inputData = <HTMLInputElement> document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement> document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement> document.querySelector('#valor');
+
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
@@ -20,14 +22,10 @@ class NegociacaoController {
         const negociacao = new Negociacao(
             new Date(this._inputData.value.replace(/-/g, ',')),
             parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value));
+            parseFloat(this._inputValor.value)
+        );
 
-            this._negociacoes.adiciona(negociacao);
-
-            this._negociacoes.paraArray().forEach(negociacao => {
-                console.log(negociacao.data);
-                console.log(negociacao.quantidade);
-                console.log(negociacao.valor);
-            });
+        this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes);
     }
 }
