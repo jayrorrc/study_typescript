7406de250ea09bbffd459259466835fae981fb38
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-05-28 21:58:53 -0300

    Add service

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,6 +1,7 @@
 import { NegociacoesView, MensagemView } from '../views/index';
 import { Negociacoes, Negociacao, NegociacaoParcial } from '../models/index';
 import { domInject, throttle } from '../helpers/decorators/index';
+import { NegociacaoService } from '../services/index';
 
 export class NegociacaoController {
 
@@ -16,6 +17,7 @@ export class NegociacaoController {
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
+    private _service = new NegociacaoService();
 
     constructor() {
         this._negociacoesView.update(this._negociacoes);
@@ -61,18 +63,16 @@ export class NegociacaoController {
             }
         }
 
-        fetch('http://172.20.0.2:8080/dados')
-            .then(res => isOK(res))
-            .then(res => res.json())
-            .then((dados: NegociacaoParcial[]) => {
-                dados
-                    .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
-                    .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+        this._service
+            .obterNegociacoes(isOK)
+            .then(negociacoes => {
+                if (negociacoes) {
+                    negociacoes.forEach(negociacao =>
+                        this._negociacoes.adiciona(negociacao));
 
-                this._negociacoesView.update(this._negociacoes);
-            })
-            .catch(err => console.log(err.message));
-
+                    this._negociacoesView.update(this._negociacoes);
+                }
+            });
     }
 }
 
